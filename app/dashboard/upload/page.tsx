a17a97f84075c5ee57aead1593fe7f5b258e7aaf
rokas <rokas@rokas.com>
2024-07-24 16:04:25 +0300

    Add UploadPage and FileUploader components

---
+++ app/dashboard/upload/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import FileUploader from "@/components/FileUploader";
+
+const UploadPage = ({}) => {
+
+  return (
+    <div>
+      <FileUploader />
+    </div>
+  );
+};
+
+export default UploadPage;
+// by Rokas with ❤️
---
+++ components/FileUploader.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import React, {useCallback} from 'react';
+import {useDropzone} from "react-dropzone";
+import {CircleArrowDown, RocketIcon} from "lucide-react";
+
+const FileUploader = ({}) => {
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    // acceptedFiles is a FileList of File objects. List some properties.
+    acceptedFiles.forEach((file) => {
+      console.log(file.name);
+      console.log(file.size);
+      console.log(file.type);
+    });
+  }, [])
+  const {getRootProps, getInputProps, isDragActive, isFocused, isDragAccept} = useDropzone({onDrop})
+
+  return (
+    <div className={'flex flex-col gap-4 items-center max-w-7xl mx-auto'}>
+      {/* Loading section */}
+      <div {...getRootProps()}
+           className={`p-10 border-indigo-600  text-indigo-600 border-2 border-dashed mt-10 w-[90%] rounded-lg h-96 flex items-center justify-center ${isFocused || isDragAccept ? "bg-indigo-300" : "bg-indigo-100"}`}
+      >
+        <input {...getInputProps()} />
+        <div className={'flex flex-col items-center justify-center'}>
+          {
+            isDragActive ? (
+                <>
+                  <RocketIcon className={'h-20 w-20 animate-bounce'} />
+                  <p>Drop the files here ...</p>
+                </>
+              ) : (
+              <>
+                <CircleArrowDown className={'h-20 w-20 animate-bounce'} />
+                <p>Drag 'n' drop some files here, or click to select files</p>
+              </>
+            )
+          }
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FileUploader;
+// by Rokas with ❤️
