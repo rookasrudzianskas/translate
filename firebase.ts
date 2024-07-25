77c654be99e214019cc532ac1a7c2683fa75349f
rokas <rokas@rokas.com>
2024-07-25 14:42:35 +0300

    Add service_key.json to gitignore and export storage

--- firebase.ts
+++ firebase.ts
@@ -16,4 +16,7 @@ const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore(app);
 const storage = getStorage(app);
 
-export {db};
+export {
+  db,
+  storage,
+};
