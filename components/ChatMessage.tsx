a904c89626e7f8ab5dd3b3d93bf901ba55d9f9cf
rokas <rokas@rokas.com>
2024-07-26 11:06:06 +0300

    Update ChatMessage component with user image display

--- components/ChatMessage.tsx
+++ components/ChatMessage.tsx
@@ -1,9 +1,46 @@
-import React from 'react';
+"use client";
 
-const ChatMessage = ({}) => {
+import React from 'react';
+import {Message} from "@/components/Chat";
+import {useUser} from "@clerk/nextjs";
+import Image from "next/image";
+import {BotIcon, Loader2Icon} from "lucide-react";
+import Markdown from "react-markdown";
+
+const ChatMessage = ({message}: { message: Message }) => {
+  const isHuman = message.role === "human";
+  const { user } = useUser();
+
   return (
-    <div>
-      
+    <div className={`chat ${isHuman ? "chat-end" : 'chat-start'}`}>
+      <div className={'chat-image avatar'}>
+        <div>
+          {isHuman ? (
+            user?.imageUrl && (
+              <Image
+                src={user?.imageUrl}
+                alt={'Profile Picture'}
+                width={40}
+                height={40}
+                className={'rounded-full'}
+              />
+            )
+          ) : (
+            <div className={'h-10 w-10 bg-indigo-600 flex items-center justify-center'}>
+              <BotIcon className={'text-white h-7 w-7'} />
+            </div>
+          )}
+        </div>
+      </div>
+      <div className={`chat-bubble prose ${isHuman && "bg-indigo-600 text-white"}`}>
+        {message.message === "Thinking..." ? (
+          <div className={'flex items-center justify-center'}>
+            <Loader2Icon className={'animate-spin h-5 w-5 text-white'} />
+          </div>
+        ) : (
+          <Markdown>{message.message}</Markdown>
+        )}
+      </div>
     </div>
   );
 };
--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias.canvas = false;
+
+    return config;
+  },
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'i.imgur.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.clerk.com',
         port: '',
         pathname: '/**',
       },
